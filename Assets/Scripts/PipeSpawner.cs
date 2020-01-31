c5a217a027f21a24e2dd21b7e8cc214d92ad84af
Cinnamon18 <alice.ava.moh@gmail.com>
2020-01-31 12:32:23 -0500

    small tweaks to player controller

--- Assets/Scripts/PipeSpawner.cs
+++ Assets/Scripts/PipeSpawner.cs
@@ -2,26 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PipeSpawner : MonoBehaviour
-{
-    public GameObject pipePrefab;
-    public float spawnTime = 2;
-    float spawnTimer;
-    public float pipeRange = 1;
-    // Start is called before the first frame update
-    void Start()
-    {
-        spawnTimer = spawnTime;
-    }
+public class PipeSpawner : MonoBehaviour {
 
-    // Update is called once per frame
-    void Update()
-    {
-        spawnTimer -= Time.deltaTime;
-        if (spawnTimer <= 0) {
-            spawnTimer = spawnTime;
-            GameObject g = Instantiate(pipePrefab, transform);
-            g.transform.position += new Vector3(0, Random.Range(-pipeRange, pipeRange), 0);
-        }
-    }
+	public GameObject pipePrefab;
+	public float spawnTime = 2;
+	float spawnTimer;
+	public float pipeRange = 1.5f;
+
+	// Start is called before the first frame update
+	void Start() {
+		spawnTimer = spawnTime;
+	}
+
+	// Update is called once per frame
+	void Update() {
+		spawnTimer -= Time.deltaTime;
+		if (spawnTimer <= 0) {
+			spawnTimer = spawnTime;
+			GameObject gameObject = Instantiate(pipePrefab, this.transform.position, pipePrefab.transform.rotation);
+			gameObject.transform.position += new Vector3(0, Random.Range(-pipeRange, pipeRange), 0);
+		}
+	}
 }
